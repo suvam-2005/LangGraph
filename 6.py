f96859056e93837fbc430c917335c821e294235a
suvam-2005 <professional.suvamdas@gmail.com>
2026-01-24 13:14:53 +0530

    W

---
+++ 6.py
@@ -0,0 +1,68 @@
+#Conditional workflow
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from pydantic import BaseModel, Field, Literal
+import operator
+
+class QuadState(TypedDict):
+    a:int
+    b:int
+    c:int
+
+    equation:str
+    discriminant:float
+    result:str
+
+def show_equation(state:QuadState):
+    equation=f'{state["a"]}x2{state["b"]}x{state["c"]}'
+    return {'equation':equation}
+
+def calculate_discriminant(state:QuadState):
+    discriminant=state["b"]**2-(4*state["a"]*state["c"])
+    return {"discriminant":discriminant}
+
+def real_roots(state:QuadState):
+    root1=(-state["b"]+state["discriminant"]**0.5)/(2*state["a"])
+    root2=(-state["b"]-state["discriminant"]**0.5)/(2*state["a"])
+
+    result=f'the roots are {root1} and {root2}'
+
+    return{'result':result}
+
+def repeted_roots(state:QuadState):
+    root=(-state["b"])/(2*state["a"])    
+    result=f'only repeting root is {root}'
+    return{'result':result} 
+
+def no_real_roots(state:QuadState):
+    result=f'No real roots'
+    return{'result':result}
+
+def check_condition(state:QuadState)->Literal["real_roots", "repeted_roots", "no_real_roots"]:
+    if state['discriminant']>0:
+        return "real_roots"
+    elif state['discriminant']==0:
+        return "repeated_roots"
+    else:
+        return "np_real_roots"
+
+graph=StateGraph(QuadState)
+
+graph.add_node('show_equation', show_equation)
+graph.add_node('calculate_discriminant', calculate_discriminant)
+graph.add_node('real_roots', real_roots)
+graph.add_node('repeted_roots', repeted_roots)
+graph.add_node('no_real_roots', no_real_roots)
+
+graph.add_edge(START,'show_equation')
+graph.add_edge('show_equation','calculate_discriminant')
+
+graph.add_conditional_edges('calculate_discriminant', check_condition)
+graph.add_edge('real_roots',END)
+graph.add_edge('repeted_roots',END)
+graph.add_edge('no_real_roots',END)
+
+workflow=graph.compile()
+
